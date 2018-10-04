05b7d9bd419bb67fda0ddaa5e7ddcffeb8387434
Gizzz <bulatkhusainov@yandex.ru>
2018-10-04 19:44:24 +0300

    Board component: make ship cells destroyable

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -11,15 +11,35 @@ class Board extends React.Component {
       "destroyer": { "size": 2, "count": 1 },
     },
     "layout": [
-      { "ship": "carrier", "positions": [[2,9], [3,9], [4,9], [5,9], [6,9]] },
-      { "ship": "battleship", "positions": [[5,2], [5,3], [5,4], [5,5]] },
-      { "ship": "cruiser", "positions": [[8,1], [8,2], [8,3]] },
-      { "ship": "submarine", "positions": [[3,0], [3,1], [3,2]] },
-      { "ship": "destroyer", "positions": [[0,0], [1,0]] },
+      { "shipType": "carrier", "positions": [[2,9,1], [3,9,1], [4,9,1], [5,9,1], [6,9,1]] },
+      { "shipType": "battleship", "positions": [[5,2,1], [5,3,1], [5,4,1], [5,5,1]] },
+      { "shipType": "cruiser", "positions": [[8,1,1], [8,2,1], [8,3,1]] },
+      { "shipType": "submarine", "positions": [[3,0,1], [3,1,1], [3,2,1]] },
+      { "shipType": "destroyer", "positions": [[0,0,1], [1,0,1]] },
     ]
   }
 
   state = this.initialState
+
+  createCellClickHandler = (rowIndex, colIndex) => {
+    return () => {
+      const xPos = colIndex;
+      const yPos = rowIndex;
+
+      const layoutCopy = JSON.parse(JSON.stringify(this.state.layout));
+      const newLayout = layoutCopy.map((ship) => {
+        return {
+          ...ship,
+          positions: ship.positions.map((pos) => {
+            const isPositionClicked = pos[0] === xPos && pos[1] === yPos;
+            return isPositionClicked ? [pos[0], pos[1], 0] : pos;
+          }),
+        };
+      })
+      
+      this.setState({ layout: newLayout });
+    };
+  }
 
   computeBoard = () => {
     const board = [];
@@ -39,7 +59,8 @@ class Board extends React.Component {
       for (let position of ship.positions) {
         const rowIndex = position[1];
         const colIndex = position[0];
-        board[rowIndex][colIndex] = '+';
+        const isPositionAlive = position[2] === 1;
+        board[rowIndex][colIndex] = isPositionAlive ? '+' : 'x';
       }
     }
 
@@ -50,13 +71,19 @@ class Board extends React.Component {
     const computedBoard = this.computeBoard();
     const rowsJsx = [];
 
-    for (let i = 0; i < computedBoard.length; i++) {
-      const row = computedBoard[i];
-      const colsJsx = row.map((cellValue, index) => (
-        <div className="board__cell" key={index}>{cellValue}</div>
+    for (let rowIndex = 0; rowIndex < computedBoard.length; rowIndex++) {
+      const row = computedBoard[rowIndex];
+      const colsJsx = row.map((cellValue, colIndex) => (
+        <div
+          className="board__cell"
+          key={colIndex}
+          onClick={this.createCellClickHandler(rowIndex, colIndex)}
+        >
+          {cellValue}
+        </div>
       ));
 
-      const rowJsx = (<div className="board__row" key={i}>{colsJsx}</div>);
+      const rowJsx = (<div className="board__row" key={rowIndex}>{colsJsx}</div>);
       rowsJsx.push(rowJsx);
     }
 
