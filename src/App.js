1b9b234e1a87216cbbec7bf6888eec73477c9f75
Gizzz <bulatkhusainov@yandex.ru>
2018-10-05 15:06:24 +0300

    styles: add responsiveness for better look on phone/tablet

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Battleship App</h1>
+        <h1 className="app__title">Battleship App</h1>
         <div className="app__board">
           <Board />
         </div>
