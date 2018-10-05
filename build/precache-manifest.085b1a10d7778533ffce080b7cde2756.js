04d3ec1a303fbb5141d2be1f8439ca96c487bddf
Gizzz <bulatkhusainov@yandex.ru>
2018-10-05 16:18:52 +0300

    add build folder

---
+++ build/precache-manifest.085b1a10d7778533ffce080b7cde2756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "438a4cbfb046edc727ab",
+    "url": "/static/js/main.438a4cbf.chunk.js"
+  },
+  {
+    "revision": "29b7954b9b8e5e159a05",
+    "url": "/static/js/1.29b7954b.chunk.js"
+  },
+  {
+    "revision": "438a4cbfb046edc727ab",
+    "url": "/static/css/main.4dbe4f4d.chunk.css"
+  },
+  {
+    "revision": "4787f81e8382e11eda478aa1744a76e9",
+    "url": "/index.html"
+  }
+];
