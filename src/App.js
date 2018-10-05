0812c0dc4b5ccb1a115cb0f809ec4c3236622816
Gizzz <bulatkhusainov@yandex.ru>
2018-10-05 15:46:13 +0300

    App comp.: add descrition text below title

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     return (
       <div className="app">
         <h1 className="app__title">Battleship App</h1>
+        <p className="app__descr">Search and destroy all enemy ships by clicking on tiles of the board.</p>
         <div className="app__board">
           <Board />
         </div>
